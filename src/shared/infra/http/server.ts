839bad603b269db034836ec350ce7d7ecc138dea
Denis Medeiros <48757658+DenisMedeirosKollosso@users.noreply.github.com>
2020-06-20 23:05:18 -0300

    basic initial server and routes

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import routes from '../http/routes'
+
+const server = express()
+
+server.use(express.json())
+server.use(routes)
+
+server.listen(3333, () => {
+  console.log('Start server development, listen port 3333')
+})
